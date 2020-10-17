a6aecfd8843bf20194e9f4d8ee8224067f8d9637
pauleeeeee <paul.embleton@gmail.com>
2020-10-17 10:42:00 -0600

    current code

--- src/c/just-chat.c
+++ src/c/just-chat.c
@@ -50,6 +50,7 @@ int msgs_get_idx(int idx){
 }
 
 MessageBubble* msgs_get(int idx){
+  printf("getting message idx %d", idx);
 	return &s_message_bubbles[msgs_get_idx(idx)];
 }
 
@@ -141,6 +142,7 @@ static void draw_message_bubbles(){
 
     //for each message, render a new bubble (stored in the text layers array), adjust the important bounds object, and update the scroll layer's size
     for(int index = msgsCount - 1; index >= 0; index--) {
+      // printf("index number %d in the for loop", index);
       //render the bubble for the message
       s_text_layers[index] = render_new_bubble(index, bounds);
       //adjust bounds based on the height of the bubble rendered
@@ -158,7 +160,7 @@ static void draw_message_bubbles(){
 
 //adds a new message to the messages array but does not render anything
 static void add_new_message(char *text, bool is_user){
-		msg_push((MessageBubble){text, strlen(text), is_user});
+		msg_push((MessageBubble){text, strlen(text) + 1, is_user});
 }
 
 
@@ -226,7 +228,10 @@ static void in_received_handler(DictionaryIterator *iter, void *context) {
   Tuple *message_text_tuple = dict_find( iter, MessageText );
   if (message_text_tuple) {
     //if an appmessage has a ActionResponse key, read the value and add a new message. Since this is the assistant, the is_user flag is set to false
-    add_new_message(message_text_tuple->value->cstring, false);
+    char* text = malloc(strlen(message_text_tuple->value->cstring) + 1);
+    strcpy(text, message_text_tuple->value->cstring);
+    add_new_message(text, false);
+    // add_new_message(message_text_tuple->value->cstring, false);
     draw_message_bubbles();
     vibes_short_pulse();
   }
